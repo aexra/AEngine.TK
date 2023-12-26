1c8cb9d219c69b5decf96361ef84f14ace570770
Никита Фомин <defdXs@yandex.ru>
2023-12-26 17:28:43 +0300

    Some rendering stuff

--- AEngine.TK/Implementations/TestGame.cs
+++ AEngine.TK/Implementations/TestGame.cs
@@ -6,6 +6,15 @@ namespace AEngine.TK;
 
 internal class TestGame : Game
 {
+    private readonly float[] _vertices = [
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    ];
+
+    private int _vertexBufferObject;
+    private int _vertexArrayObject;
+
     public TestGame(string windowTitle, int initialWindowWidth, int initialWindowHeight) : base(windowTitle, initialWindowWidth, initialWindowHeight)
     {
     }
@@ -17,7 +26,15 @@ internal class TestGame : Game
 
     protected override void LoadContent()
     {
-        
+        _vertexBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+        GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.DynamicDraw);
+
+        _vertexArrayObject = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayObject);
+
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
     }
 
     protected override void Update(GameTime gameTime)
