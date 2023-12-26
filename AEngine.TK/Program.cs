4ceb60489495ee790c88f772300702413476f68a
Никита Фомин <defdXs@yandex.ru>
2023-12-26 16:27:19 +0300

    Nice opening window

--- AEngine.TK/Program.cs
+++ AEngine.TK/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace AEngine.TK;
+﻿using AEngine.TK.Core;
+
+namespace AEngine.TK;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        
+        Game game = new TestGame("Test", 800, 600);
+        game.Run();
     }
 }
