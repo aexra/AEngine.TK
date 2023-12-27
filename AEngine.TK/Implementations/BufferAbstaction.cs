9e2175334bda9660bab0c19df4c52e172d7e4aff
Никита Фомин <defdXs@yandex.ru>
2023-12-27 12:33:14 +0300

    Some ehh

---
+++ AEngine.TK/Implementations/BufferAbstaction.cs
@@ -0,0 +1,77 @@
+﻿using AEngine.TK.Core;
+using AEngine.TK.Core.Rendering;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+
+namespace AEngine.TK;
+
+internal class BufferAbstraction : Game
+{
+    private readonly float[] _vertices = [
+        -0.5f,
+        -0.5f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        0.5f,
+        -0.5f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.0f,
+        0.5f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+    ];
+
+    private int _vertexBufferObject;
+    private int _vertexArrayObject;
+
+    private Shader _shader;
+
+    public BufferAbstraction(string windowTitle, int initialWindowWidth, int initialWindowHeight) : base(windowTitle, initialWindowWidth, initialWindowHeight)
+    {
+    }
+
+    protected override void Initialize()
+    {
+
+    }
+
+    protected override void LoadContent()
+    {
+        _shader = new Shader("Resources/Shaders/default.glsl");
+
+        _vertexBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+        GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.DynamicDraw);
+
+        _vertexArrayObject = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayObject);
+
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+
+        GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
+        GL.EnableVertexAttribArray(1);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+
+    }
+
+    protected override void Render(GameTime gameTime)
+    {
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        GL.ClearColor(Color4.CornflowerBlue);
+        _shader.Use();
+        GL.BindVertexArray(_vertexArrayObject);
+        GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+    }
+}
+
