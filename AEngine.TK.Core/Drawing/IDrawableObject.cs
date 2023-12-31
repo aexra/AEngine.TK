e656d45e313465fd2ddb57dc2f51abfece8b25e0
Никита Фомин <defdXs@yandex.ru>
2023-12-31 19:40:50 +0300

    better

--- AEngine.TK.Core/Drawing/IDrawableObject.cs
+++ AEngine.TK.Core/Drawing/IDrawableObject.cs
@@ -8,12 +8,13 @@ using System.Threading.Tasks;
 using OpenTK.Graphics.OpenGL4;
 using AEngine.TK.Core.Rendering;
 using System.Numerics;
+using AEngine.TK.Core.Utils;
 
 namespace AEngine.TK.Core.Drawing
 {
     public abstract class IDrawableObject
     {
-        protected Vector2 size;
+        public Transform transform;
         protected float[] Vertices;
         protected uint[] Indices;
         protected Shader Shader;
@@ -21,9 +22,24 @@ namespace AEngine.TK.Core.Drawing
         protected VertexBuffer VertexBuffer;
         protected IndexBuffer IndexBuffer;
 
-        IDrawableObject() 
+        public IDrawableObject()
+        {
+            transform = new Transform();
+        }
+
+        public IDrawableObject(Vector3 position) : this() 
+        {
+            transform.Position = position;  
+        }
+
+        public IDrawableObject(Vector3 position, Vector3 size) : this(position) 
         { 
-            
+            transform.Size = size;
+        }
+
+        public IDrawableObject(Vector3 position, Vector3 size, Quaternion rotation) : this(position, size)
+        {
+            transform.Rotation = rotation;
         }
 
         protected abstract void Load();
---
+++ AEngine.TK.Core/Utils/Transform.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AEngine.TK.Core.Utils
+{
+    public class Transform
+    {
+        public Vector3 Position = Vector3.Zero;
+        public Vector3 Size = Vector3.Zero;
+        public Quaternion Rotation = Quaternion.Zero;
+    }
+}
