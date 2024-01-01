7cdf2d84c41eb6c715ad1250e5d54959e62ef558
Никита Фомин <defdXs@yandex.ru>
2024-01-01 19:24:01 +0300

    ref

--- AEngine.TK.Core/Engine/Tree.cs
+++ AEngine.TK.Core/Engine/Tree.cs
@@ -6,78 +6,77 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace AEngine.TK.Core.Engine
+namespace AEngine.TK.Core.Engine;
+
+public class Tree
 {
-    public class Tree
+    private static List<GameObject> gameObjects = new List<GameObject>();
+
+    public static GameObject Add(GameObject gameObject)
     {
-        private static List<GameObject> gameObjects = new List<GameObject>();
+        gameObjects.Add(gameObject);
+        return gameObject;
+    }
 
-        public static GameObject Add(GameObject gameObject)
-        {
-            gameObjects.Add(gameObject);
-            return gameObject;
+    public static GameObject Remove(GameObject gameObject)
+    {
+        gameObjects.Remove(gameObject);
+        return gameObject;
+    }
+
+    public static List<GameObject> GetAll()
+    {
+        return gameObjects;
+    }
+
+    public static GameObject? Find(string name)
+    {
+        foreach (GameObject obj in gameObjects) 
+        { 
+            if (obj.name == name) return obj;
         }
+        return null;
+    }
 
-        public static GameObject Remove(GameObject gameObject)
+    public static string GetUniqueObjectName()
+    {
+        uint lastIdx = 0;
+        List<string> uniqNums = new List<string>();
+
+        foreach (GameObject obj in gameObjects)
         {
-            gameObjects.Remove(gameObject);
-            return gameObject;
-        }
-
-        public static List<GameObject> GetAll()
-        {
-            return gameObjects;
-        }
-
-        public static GameObject? Find(string name)
-        {
-            foreach (GameObject obj in gameObjects) 
-            { 
-                if (obj.name == name) return obj;
-            }
-            return null;
-        }
-
-        public static string GetUniqueObjectName()
-        {
-            uint lastIdx = 0;
-            List<string> uniqNums = new List<string>();
-
-            foreach (GameObject obj in gameObjects)
+            if (obj.name.Contains("GameObject"))
             {
-                if (obj.name.Contains("GameObject"))
-                {
-                    uniqNums.Add(obj.name);
-                    lastIdx++;
-                }
-            }
-
-            while (uniqNums.Contains($"GameObject ({lastIdx})"))
-            {
+                uniqNums.Add(obj.name);
                 lastIdx++;
             }
-
-            if (lastIdx == 0) return "GameObject";
-            else return $"GameObject ({lastIdx})";
         }
 
-        public static void Update()
+        while (uniqNums.Contains($"GameObject ({lastIdx})"))
         {
-            foreach (GameObject obj in gameObjects)
-            {
-                obj.Update();
-            }
+            lastIdx++;
         }
 
-        public static void Draw()
-        {
-            foreach (GameObject obj in gameObjects)
-            {
-                obj.Draw();
-            }
-        }
-
-        public static GameObject First() => gameObjects.First();
-        public static GameObject Last() => gameObjects.Last();
+        if (lastIdx == 0) return "GameObject";
+        else return $"GameObject ({lastIdx})";
     }
+
+    public static void Update()
+    {
+        foreach (GameObject obj in gameObjects)
+        {
+            obj.Update();
+        }
+    }
+
+    public static void Draw()
+    {
+        foreach (GameObject obj in gameObjects)
+        {
+            obj.Draw();
+        }
+    }
+
+    public static GameObject First() => gameObjects.First();
+    public static GameObject Last() => gameObjects.Last();
 }
