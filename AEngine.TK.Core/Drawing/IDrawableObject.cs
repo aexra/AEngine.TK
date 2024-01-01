eb02936b717d3401f6cdc2eb149a44b3b57ae84c
Никита Фомин <defdXs@yandex.ru>
2024-01-01 20:41:04 +0300

    death

--- AEngine.TK.Core/Drawing/IDrawableObject.cs
+++ AEngine.TK.Core/Drawing/IDrawableObject.cs
@@ -26,7 +26,7 @@ public abstract class IDrawableObject : GameObject
 
     public IDrawableObject() : base()
     {
-        
+       
     }
 
     public IDrawableObject(Vector3 position) : this() 
