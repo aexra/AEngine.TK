f994fc4000f8240be408dff38e903fe615e19213
Никита Фомин <defdXs@yandex.ru>
2024-01-10 21:19:55 +0300

    eeeeeeeeeeeeeeeeeeeeeeehh

--- AEngine.TK.Core/Utils/GameObject.cs
+++ AEngine.TK.Core/Utils/GameObject.cs
@@ -1,12 +1,14 @@
 ﻿using AEngine.TK.Core.Engine;
 using AEngine.TK.Core.Rendering;
 using OpenTK.Graphics.ES11;
+using OpenTK.Mathematics;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace AEngine.TK.Core.Utils;
 
@@ -35,6 +37,79 @@ public abstract class GameObject
     public void Hide() { Visible = false; }
     public void Activate() { Active = true; }
     public void Deactivate() { Active = false; }
+
+    public void Translate(float dx, float dy, float dz)
+    {
+        transform.Translate(dx, dy, dz);
+        foreach (var child in Children)
+        {
+            child.Translate(dx, dy, dz);
+        }
+    }
+    public void Translate(Vector3 d)
+    {
+        transform.Translate(d);
+        foreach (var child in Children)
+        {
+            child.Translate(d);
+        }
+    }
+    public void TranslateTo(float x, float y, float z)
+    {
+        float dx = x - transform.position.X, 
+            dy = y - transform.position.Y, 
+            dz = z - transform.position.Z;
+        transform.TranslateTo(x, y, z);
+        foreach (var child in Children)
+        {
+            child.Translate(dx, dy, dz);
+        }
+    }
+    public void TranslateTo(Vector3 to)
+    {
+        Vector3 d = to - transform.position;
+        transform.TranslateTo(to);
+        foreach (var child in Children)
+        {
+            child.Translate(d);
+        }
+    }
+    public void Scale(float dsx, float dsy, float dsz)
+    {
+        transform.Scale(dsx, dsy, dsz);
+        foreach (var child in Children)
+        {
+            child.Scale(dsx, dsy, dsz);
+        }
+    }
+    public void Scale(Vector3 ds)
+    {
+        transform.Scale(ds);
+        foreach (var child in Children)
+        {
+            child.Scale(ds);
+        }
+    }
+    public void ScaleTo(float sx, float sy, float sz)
+    {
+        float dsx = sx - transform.scale.X,
+            dsy = sy - transform.scale.Y,
+            dsz = sz - transform.scale.Z;
+        transform.ScaleTo(sx, sy, sz);
+        foreach (var child in Children)
+        {
+            child.Scale(dsx, dsy, dsz);
+        }
+    }
+    public void ScaleTo(Vector3 to)
+    {
+        Vector3 d = to - transform.scale;
+        transform.ScaleTo(to);
+        foreach (var child in Children)
+        {
+            child.Scale(d);
+        }
+    }
 
     public abstract void Update();
     public abstract void Draw();
--- AEngine.TK.Core/Utils/Transform.cs
+++ AEngine.TK.Core/Utils/Transform.cs
@@ -19,4 +19,53 @@ public class Transform
                 Matrix4.CreateScale(scale.X, scale.Y, scale.Z);
         }
     }
+
+    public void Translate(float dx, float dy, float dz)
+    {
+        position.X += dx;
+        position.Y += dy;
+        position.Z += dz;
+    }
+    public void Translate(Vector3 d)
+    {
+        position.X += d.X;
+        position.Y += d.Y;
+        position.Z += d.Z;
+    }
+    public void TranslateTo(float x, float y, float z)
+    {
+        position.X = x;
+        position.Y = y;
+        position.Z = z;
+    }
+    public void TranslateTo(Vector3 to)
+    {
+        position.X = to.X;
+        position.Y = to.Y;
+        position.Z = to.Z;
+    }
+    public void Scale(float dsx, float dsy, float dsz)
+    {
+        scale.X += dsx;
+        scale.Y += dsy;
+        scale.Z += dsz;
+    }
+    public void Scale(Vector3 ds)
+    {
+        scale.X += ds.X;
+        scale.Y += ds.Y;
+        scale.Z += ds.Z;
+    }
+    public void ScaleTo(float sx, float sy, float sz)
+    {
+        scale.X = sx;
+        scale.Y = sy;
+        scale.Z = sz;
+    }
+    public void ScaleTo(Vector3 to)
+    {
+        scale.X = to.X;
+        scale.Y = to.Y;
+        scale.Z = to.Z;
+    }
 }
